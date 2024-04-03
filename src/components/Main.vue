8672b78d4795a335fce557c64d816f0bcad45c03
NicolaBellini <bellininicola4366@gmail.com>
2024-04-03 15:58:27 +0200

    [add] print in page of title in a card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,25 @@
 <script>
 import { store } from "../data/store";
+import Card from "./partials/Card.vue";
 export default {
   data() {
     return {
       store,
     };
   },
+  components: {
+    Card,
+  },
 };
 </script>
 
 <template>
   <div class="text-center my-5 container">
-    film <br />
-    {{ this.store.movieList }} <br />
-    serietv <br />
-    {{ this.store.tvList }}
+    <Card
+      :element="element"
+      v-for="(element, index) in this.store.tvList"
+      :key="index"
+    />
   </div>
 </template>
 
---
+++ src/components/partials/Card.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    element: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ element.name }}</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
